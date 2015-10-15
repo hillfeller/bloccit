c1d02652209aec2ed5531536ef44ee026d9bca00
Hillary <hillfeller@gmail.com>
2015-10-15 17:34:01 -0400

    Assignment 34 Rails Crud completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'advertisement/index'
+
+  get 'advertisement/show'
+
+  get 'advertisement/new'
+
+  get 'advertisement/create'
+
   resources :posts
+  resources :advertisement
 
   get 'about' => 'welcome#about'
   root to: 'welcome#index'
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,17 @@ include RandomData
    )
  end
 
- puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+
+ 50.times do
+ # #1
+   Advertisement.create!(
+ # #2
+     title:  RandomData.random_sentence,
+     copy:   RandomData.random_paragraph,
+     price:  RandomData.random_sentence
+   )
+ end
+advertisement = Advertisement.all
+puts "#{Advertisement.count} advertisements created"
