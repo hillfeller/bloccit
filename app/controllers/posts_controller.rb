c3ba1f8bed3948e9a7e95fa7db7690b1957e6e6e
Hillary <hillfeller@gmail.com>
2015-10-15 14:38:32 -0400

    Assignment 34

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each_with_index{|post, index|
+      if index % 5 == 0
+        new_title = post.title.replace "SPAM!"
+      end
+    }
   end
 
   def show
