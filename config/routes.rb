0c57705714803a26291023cf9361ee9acdb77419
Hillary <hillfeller@gmail.com>
2015-10-19 11:48:36 -0400

    Resolved checkbox update

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,4 @@
 Rails.application.routes.draw do
-  get 'questions/index'
-
-  get 'questions/new'
-
-  get 'questions/create'
-
-  get 'questions/show'
-
-  get 'questions/edit'
-
-  get 'questions/update'
-
-  get 'questions/destroy'
 
   resources :posts
   resources :questions
