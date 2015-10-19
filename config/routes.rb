fbb0f9055580993e51f77519557dc31167ec0631
Hillary <hillfeller@gmail.com>
2015-10-19 19:00:04 -0400

    Checkpoint 36 Authentication completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :posts
-  
+
   resources :topics do
     resources :posts, except: [:index]
   end
 
+  resources :users, only: [:new, :create]
   get 'about' => 'welcome#about'
   root to: 'welcome#index'
 
