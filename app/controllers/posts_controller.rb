113d3fd977d383ec68e969f53566630de7757ef8
Hillary Feller <hillfeller@gmail.com>
2015-10-18 20:23:14 -0400

    Checkpoint 34 Topics and Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,11 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
 
   def show
     @post = Post.find(params[:id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
   end
 
@@ -16,13 +14,17 @@ class PostsController < ApplicationController
     @post = Post.new
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
+    @topic = Topic.find(params[:topic_id])
+# #35
+    @post.topic = @topic
 
     if @post.save
-       flash[:notice] = "Post was saved."
-       redirect_to @post
+      flash[:notice] = "Post was saved."
+# #36
+      redirect_to [@topic, @post]
     else
-       flash[:error] = "There was an error saving the post. Please try again."
-       render :new
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
     end
   end
 
@@ -37,7 +39,7 @@ class PostsController < ApplicationController
 
     if @post.save
       flash[:notice] = "Post was updated."
-      redirect_to @post
+      redirect_to [@post.topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :edit
@@ -49,7 +51,7 @@ class PostsController < ApplicationController
 
     if @post.destroy
       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-      redirect_to posts_path
+      redirect_to @post.topic
     else
       flash[:error] = "There was an error deleting the post."
       render :show
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
+include RandomData
 
 RSpec.describe Post, type: :model do
+# #3
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+# #4
+  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
-  let(:post) { Post.create!(title: "New Post Title", body: "New Post Body") }
+  it { should belong_to(:topic) }
 
   describe "attributes" do
-
     it "should respond to title" do
       expect(post).to respond_to(:title)
     end
