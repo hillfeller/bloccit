c83e24cd8759314a8a71335c4e718d0b414a6f45
Hillary <hillfeller@gmail.com>
2015-10-19 17:39:31 -0400

    Assignment 35 validations completed

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -6,7 +6,13 @@ RSpec.describe Topic, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
 
   it { should have_many(:posts) }
-# #1
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:public) }
+  it { should validate_length_of(:name).is_at_least(5) }
+  it { should validate_length_of(:description).is_at_least(15) }
+
   describe "attributes" do
     it "should respond to name" do
       expect(topic).to respond_to(:name)
@@ -20,7 +26,6 @@ RSpec.describe Topic, type: :model do
       expect(topic).to respond_to(:public)
     end
 
-# #2
     it "should be public by default" do
       expect(topic.public).to be(true)
     end
