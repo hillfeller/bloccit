c50331c6e6a8ec0f777e65b0a3bdc1d5f3c50d91
Hillary <hillfeller@gmail.com>
2015-10-19 16:51:53 -0400

    Checkpoint 35 Validations completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,10 +7,12 @@ RSpec.describe Post, type: :model do
 # #4
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
-  it { should belong_to(:topic) }
+   it { should belong_to(:topic) }
+
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
   it { should validate_presence_of(:topic) }
+# 2
   it { should validate_length_of(:title).is_at_least(5) }
   it { should validate_length_of(:body).is_at_least(20) }
 
