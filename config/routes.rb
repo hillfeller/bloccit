554272059dfb4e40896d9d5b2218b771e66e9756
Hillary <hillfeller@gmail.com>
2015-10-22 18:25:41 -0400

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
 
+  resources :posts
+
   resources :topics do
     resources :posts, except: [:index]
   end
 
+  resources :users, only: [:new, :create]
 
-  resources :posts, only: [] do
-    resources :comments, only: [:create, :destroy]
-  end
+  resources :sessions, only: [:new, :create, :destroy]
 
+  get 'about' => 'welcome#about'
+
+  root to: 'welcome#index'
 end
