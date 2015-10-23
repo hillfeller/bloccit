506abfea1b3383737e19d8ff0ffa380d8a4cc69b
Hillary <hillfeller@gmail.com>
2015-10-22 23:00:27 -0400

    Updates to checkpoint 42

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 Rails.application.routes.draw do
 
+  resources :labels, only: [:show]
+  
+  resources :posts, only: [] do
+     resources :comments, only: [:create, :destroy]
+   end
+
   resources :topics do
     resources :posts, except: [:index]
   end
 
+  resources :users, only: [:new, :create]
+
+  resources :sessions, only: [:new, :create, :destroy]
+
   get 'about' => 'welcome#about'
+
   root to: 'welcome#index'
 
-  resources :posts, only: [] do
-    resources :comments, only: [:create, :destroy]
-  end
 
-  resources :labels, only: [:show]
-  end
+end
